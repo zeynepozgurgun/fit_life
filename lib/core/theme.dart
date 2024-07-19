d2f95b995cf3a62581858048f0711c1ff6031139
zeynepozgurgun <127887754+zeynepozgurgun@users.noreply.github.com>
2024-07-19 11:31:22 +0300

    Feature/Fitlife-3 Create theme file issue (#6)
    * remove single comment
    
    * deleted comments
    
    * Update main.dart
    
    * theme.dart added
    
    * theme commit
    
    * Theme-file-and-signup-screen
    
    - theme file updated with google fonts and colors instance made private
    - a signup screen (reached from a placeholder login screen) added
    
    * "fontfamily:roboto"s removed
    
    ---------
    
    Co-authored-by: Okan Demir <okandemir3358@gmail.com>
    Co-authored-by: Okan Demir <43478072+okandemirofficial@users.noreply.github.com>

---
+++ lib/core/theme.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+const ColorScheme _colors = const ColorScheme(
+  primary: const Color(0xFFf1f7d7),
+  onPrimary: const Color(0xFFfbe3a3),
+  secondary: const Color(0xFF206380),
+  onSecondary: const Color(0xFF154459),
+  surface: const Color(0xFF39bebe),
+  error: const Color(0xffF9371D),
+  onPrimaryContainer: const Color(0xffA2AAAB),
+  onError: const Color(0xFF9AFFFF),
+  onSurface: const Color(0xFF49454F),
+  brightness: Brightness.light,
+);
+
+final ThemeData appTheme = ThemeData(
+  colorScheme: _colors,
+  fontFamily: GoogleFonts.roboto().fontFamily,
+  textTheme: TextTheme(
+    titleLarge: TextStyle( //APP NAME
+      fontSize: 30.0,
+      fontWeight: FontWeight.w800,
+      color: _colors.onSecondary,
+    ),
+  
+    titleMedium: TextStyle( //WELCOME TEXT + Setting options
+    fontSize: 18.0,
+    fontWeight: FontWeight.w500,
+    color: _colors.onSecondary),
+
+    titleSmall: TextStyle( //LOGIN / SETTINGS / MY ACCOUNT
+    fontSize: 25.0,
+    fontWeight: FontWeight.w800,
+    color: _colors.secondary),
+
+    headlineSmall: TextStyle( //username: + cont without an acc. + warnings
+    fontSize: 17.0,
+    fontWeight: FontWeight.w500,
+    color: _colors.secondary,),
+
+    headlineMedium: TextStyle( //you havent signed up to an account
+    fontSize: 16.0,
+    fontWeight: FontWeight.w500,
+    color: _colors.secondary,),
+
+    headlineLarge: TextStyle( //Labels for personal info (height weight etc.)
+    fontSize: 18.0,
+    fontWeight: FontWeight.w600,
+    color: _colors.onPrimary),
+
+    displayLarge: TextStyle( //DIET HEADLINES
+    fontSize: 24.0,
+    fontWeight: FontWeight.w800,
+    color: _colors.onSecondary),
+
+    displayMedium: TextStyle( //DIET DETAIL HEADLINES
+    fontSize: 20.0,
+    fontWeight: FontWeight.w500,
+    color: _colors.onSecondary),
+  
+    displaySmall: TextStyle( //DIET info + personal info + username password enter
+    fontSize: 15.0,
+    fontWeight: FontWeight.w400,
+    color: _colors.onSurface),
+
+    bodyLarge: TextStyle( //choose all that apply / search for keywords
+    fontSize: 14.0,
+    fontWeight: FontWeight.normal,
+    fontStyle: FontStyle.italic,
+    color: _colors.onPrimaryContainer,
+    ),
+
+    bodyMedium: TextStyle( //edit / done
+    fontSize: 14.0,
+    fontWeight: FontWeight.normal,
+    fontStyle: FontStyle.italic,
+    color: _colors.primary,
+    ),
+
+    bodySmall: TextStyle( //credits
+    fontSize: 10.0,
+    fontWeight: FontWeight.w400,
+    fontStyle: FontStyle.italic,
+    color: _colors.onError,
+    )
+  )
+);
