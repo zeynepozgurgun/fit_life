bc95b789a5fa7b184f69c1385dd40bb4ae67ed55
zeynepozgurgun <zeynepozgur@sabanciuniv.edu>
2024-08-02 15:02:45 +0300

    imports

--- lib/data/diet_service.dart
+++ lib/data/diet_service.dart
@@ -1,5 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'Diet.dart';  
+import 'diet.dart';  
 
 class DietService {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
--- lib/home/diet_details.dart
+++ lib/home/diet_details.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:fit_life/data/Diet.dart';
+import 'package:fit_life/data/diet.dart';
 
 class DietDetailScreen extends StatelessWidget {
   final Diet diet;
